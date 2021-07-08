202d01e1ee8a0ac03ca1aeb1516852429bbf66a9
Jeffrey <jeffrey.tan@illumina.com>
2021-07-08 17:26:00 +0800

    Event binding
    Remove title and logo
    Add blur, input and click event

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,21 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
-  template: `
+  template: /* html */ `
     <div class="app">
-      <h1 [innerHTML]="title"></h1>
-      <h1>{{ title }}</h1>
-      <img [src]="logo">
-      <input type="text" [value]="name">
+      <button (click)="handleClick()">
+        Change name
+      </button>
+      <input
+        type="text"
+        [value]="name"
+        (input)="handleInput($event)"
+        (blur)="handleBlur($event)"
+      >
       <div>{{ name }}</div>
     </div>
   `
 })
 export class AppComponent {
-  title: string;
-  name: string = 'Todd';
-  logo: string = 'img/logo.svg';
-  constructor() {
-    this.title = 'Ultimate Angular'
+  name: string = 'Jeff';
+  handleClick() {
+    this.name = 'Tan';
+  }
+  handleInput(event: any) {
+    this.name = event.target.value;
+  }
+  handleBlur(event: any) {
+    this.name = event.target.value;
+    console.log(event);
   }
 }
