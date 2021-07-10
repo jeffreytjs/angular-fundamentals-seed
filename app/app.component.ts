d68d468342b78fd3dce60b6b96644748869db35f
Jeffrey <jeffrey.tan@illumina.com>
2021-07-10 15:54:08 +0800

    ngClass and className bindings
    Airline example
    Add status scss

--- app/app.component.ts
+++ app/app.component.ts
@@ -14,17 +14,25 @@ interface Passenger {
       <h3>Airline Passengers</h3>
       <ul>
         <li *ngFor="let passenger of passengers; let i = index;">
+          <span
+            class="status"
+            [class.checked-in]="passenger.checkedIn">
+          </span>
           {{ i }}: {{ passenger.fullname }}
         </li>
       </ul>
 
       <h3>Airline Passengers</h3>
       <ul>
-        <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
-          <li>
-            {{ i }}: {{ passenger.fullname }}
-          </li>
-        </template>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span
+            class="status"
+            [ngClass]="{
+              'checked-in': passenger.checkedIn,
+              'checked-out': !passenger.checkedIn
+            }"></span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
       </ul>
     </div>
   `
