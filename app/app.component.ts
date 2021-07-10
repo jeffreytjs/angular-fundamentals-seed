640d1afd07b7fab595a6a627ccc5b603c4dbc36f
Jeffrey <jeffrey.tan@illumina.com>
2021-07-10 17:07:18 +0800

    ngStyle and style bindings

--- app/app.component.ts
+++ app/app.component.ts
@@ -21,7 +21,6 @@ interface Passenger {
           {{ i }}: {{ passenger.fullname }}
         </li>
       </ul>
-
       <h3>Airline Passengers</h3>
       <ul>
         <li *ngFor="let passenger of passengers; let i = index;">
@@ -30,6 +29,32 @@ interface Passenger {
             [ngClass]="{
               'checked-in': passenger.checkedIn,
               'checked-out': !passenger.checkedIn
+            }"></span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span
+            class="status"
+            [style.backgroundColor]="passenger.checkedIn
+              ? '#2ecc71'
+              : '#c0392b'">
+          </span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span
+            class="status"
+            [ngStyle]="{
+              backgroundColor: passenger.checkedIn
+              ? '#2ecc71'
+              : '#c0392b'
             }"></span>
           {{ i }}: {{ passenger.fullname }}
         </li>
