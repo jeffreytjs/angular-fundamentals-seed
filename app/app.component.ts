816f39ac968a2570a1be9c14bd76d0f727ffb527
Jeffrey <jeffrey.tan@illumina.com>
2021-07-18 23:03:43 +0800

    Pipes for data transformation

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 interface Passenger {
   id: number,
   fullname: string,
-  checkedIn: boolean
+  checkedIn: boolean,
+  checkInDate?: number | null
 }
 
 @Component({
@@ -14,49 +15,17 @@ interface Passenger {
       <h3>Airline Passengers</h3>
       <ul>
         <li *ngFor="let passenger of passengers; let i = index;">
-          <span
+<span
             class="status"
-            [class.checked-in]="passenger.checkedIn">
-          </span>
+            [class.checked-in]="passenger.checkedIn"></span>
           {{ i }}: {{ passenger.fullname }}
-        </li>
-      </ul>
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index;">
-          <span
-            class="status"
-            [ngClass]="{
-              'checked-in': passenger.checkedIn,
-              'checked-out': !passenger.checkedIn
-            }"></span>
-          {{ i }}: {{ passenger.fullname }}
-        </li>
-      </ul>
-
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index;">
-          <span
-            class="status"
-            [style.backgroundColor]="passenger.checkedIn
-              ? '#2ecc71'
-              : '#c0392b'">
-          </span>
-          {{ i }}: {{ passenger.fullname }}
-        </li>
-      </ul>
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index;">
-          <span
-            class="status"
-            [ngStyle]="{
-              backgroundColor: passenger.checkedIn
-              ? '#2ecc71'
-              : '#c0392b'
-            }"></span>
-          {{ i }}: {{ passenger.fullname }}
+          <p>{{ passenger | json }}</p>
+          <div class="date">
+            Check in date:
+            {{ passenger.checkInDate
+              ? (passenger.checkInDate | date: 'd MMM, y' | uppercase)
+              : 'Not checked in' }}
+          </div>
         </li>
       </ul>
     </div>
@@ -66,22 +35,27 @@ export class AppComponent {
   passengers: Passenger[] = [{
     id: 1,
     fullname: 'Stephen',
-    checkedIn: true
+    checkedIn: true,
+    checkInDate: 1490742000000
   }, {
     id: 2,
     fullname: 'Rose',
-    checkedIn: false
+    checkedIn: false,
+    checkInDate: null
   }, {
     id: 3,
     fullname: 'James',
-    checkedIn: true
+    checkedIn: true,
+    checkInDate: 1491606000000
   }, {
     id: 4,
     fullname: 'Louise',
-    checkedIn: true
+    checkedIn: true,
+    checkInDate: 1488412800000
   }, {
     id: 5,
     fullname: 'Tina',
-    checkedIn: false
+    checkedIn: false,
+    checkInDate: null
   }];
 }
